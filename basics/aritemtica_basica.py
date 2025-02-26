00573b172c7b564bdcb4acfc0c2a13dca5e551d7
AlexRamosGlz <joralexfx@gmail.com>
2025-02-26 13:05:31 -0600

    Nuevas notas agregadas

---
+++ basics/aritemtica_basica.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#   Potencias 
+print(2**5) # -> 32
+
+#   Raices
+print(4**(1/2)) # -> 2
+
+#   Floor division: regresa el parte entera de la division
+print(3//2) # -> 1
+
+#   Modulo, regresa la parte restante de la divison
+print(6%7) # -> 6
+
