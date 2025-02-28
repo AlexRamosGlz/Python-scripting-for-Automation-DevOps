3482872c819978f571e9a8778b48f6242b529a7f
AlexRamosGlz <joralexfx@gmail.com>
2025-02-28 13:21:17 -0600

    archivo 'functions.py creado'

---
+++ basics/functions.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def greeting(name: str, department: str) -> str:
+    print("Welcome, " + name)
+    print("You are part of " + department)
+
+greeting("Tere", "Veterinaria")
+greeting("Jorge", "DevOps")
