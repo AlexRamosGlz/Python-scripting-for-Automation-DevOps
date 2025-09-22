c1b6b9f470df4069ddde0d6742820bb24002b91e
AlexRamosGlz <joralexfx@gmail.com>
2025-09-22 16:54:43 -0600

    Notas agregadas
     - Basicos de OOP
     - Clases

---
+++ OOP/clases.py
@@ -0,0 +1,12 @@
+class Person:
+    
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        
+    def greet(self):
+        print(f"Hola me llamo {self.name} y tengo {self.age} años")
+        
+
+tere = Person("Tere", 25)
+tere.greet()
